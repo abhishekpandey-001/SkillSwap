b1f49fe3d6da71e50589e77ce732ff6bdfbedd00
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2026-05-31 19:16:50 +0530

    added userscema and connected the databse with the server

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    if(!process.env.MONGO_URI){
+        console.log("MongoDB URI is not present")
+        process.exit(1)
+    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const cors = require('cors')
-const dotenv = require('dotenv')
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 // Health Check
-app.get('/', (req, res)=>{
-    res.send("SkillSwap API is running")
-})
+app.get("/", (req, res) => {
+  res.send("SkillSwap API is running");
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT: ${PORT}`)
-})
+// Connect to MongoDB and start the server
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started on PORT: ${process.env.PORT}`);
+  });
+};
+
+startServer();
